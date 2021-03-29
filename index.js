3077f1c4e7fa6ecfc42092cec8e7591037adb34e
Dzianis Tkachenka <74364128+Denyto@users.noreply.github.com>
2021-03-29 21:20:41 +0300

    refactor: Improved the algorithm

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 let panels = document.querySelectorAll(".panel");
 
-panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
-panels.forEach((panel) => panel.addEventListener("click", toggleActive));
+panels.forEach((panel) => panel.addEventListener("click", toggleOpenActive));
 
-function toggleOpen() {
-  if (this.className.includes("open")) {
+function toggleOpenActive() {
+  if (this.className.includes("open") || this.className.includes("open-active")) {
     this.classList.remove("open");
-    return;
-  }
-  panels.forEach((panel) => panel.classList.remove("open"));
-  this.classList.toggle("open");
-}
-
-function toggleActive() {
-  if (this.className.includes("open-active")) {
     this.classList.remove("open-active");
     return;
   }
-  panels.forEach((panel) => panel.classList.remove("open-active"));
+  panels.forEach((panel) => {
+    panel.classList.remove("open"); panel.classList.remove("open-active")
+  });
+  this.classList.toggle("open");
   this.classList.toggle("open-active");
 }
