7077412758b90f9874b9d05123231c58ae4e2e06
Dzianis Tkachenka <74364128+Denyto@users.noreply.github.com>
2021-04-11 21:52:35 +0300

    refactor: shorten the code

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ panels.forEach((panel) => panel.addEventListener("click", toggleOpenActive));
 
 function toggleOpenActive() {
   if (this.className.includes("open") || this.className.includes("open-active")) {
-    this.classList.remove("open");
-    this.classList.remove("open-active");
+    this.classList.remove("open", "open-active");
     return;
   }
   panels.forEach((panel) => {
-    panel.classList.remove("open"); panel.classList.remove("open-active")
+    panel.classList.remove("open", "open-active");
   });
   this.classList.toggle("open");
   this.classList.toggle("open-active");
