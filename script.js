982dec8ff55baf1b39765dccb8caba4949430c28
Dzianis Tkachenka <74364128+Denyto@users.noreply.github.com>
2021-04-21 20:52:38 +0300

    refactor: optimize

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
-const progressBar = player.querySelector(".progress__filled");
-const toggle = player.querySelector(".toggle");
-const skipButtons = player.querySelectorAll("[data-skip]");
-const ranges = player.querySelectorAll(".player__slider");
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.slider');
 
 function togglePlay() {
     const metod = video.paused ? 'play' : 'pause';
-video[metod]();
+    video[metod]();
 }
 
 function updateButton() {
-const icon = this.paused ? "▶" : "❙❙";
-toggle.textContent = icon;
+    const icon = this.paused ? '▶' : '❙❙';
+    toggle.textContent = icon;
 }
 
 function skip() {
@@ -21,7 +21,7 @@ function skip() {
 }
 
 function handleRangeUpdate() {
-  video[this.name] = this.value;
+    video[this.name] = this.value;
 }
 
 function handleProgress() {
@@ -34,20 +34,20 @@ function scrub(e) {
     video.currentTime = scrubTime;
 }
 
-video.addEventListener("click", togglePlay);
-video.addEventListener("play", updateButton);
-video.addEventListener("pause", updateButton);
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
-toggle.addEventListener("click", togglePlay);
+toggle.addEventListener('click', togglePlay);
 
 skipButtons.forEach(button => button.addEventListener('click', skip))
-ranges.forEach((range) => {
-    range.addEventListener("change", handleRangeUpdate);
-    range.addEventListener("mousemove", handleRangeUpdate);
+ranges.forEach(range => {
+    range.addEventListener('change', handleRangeUpdate);
+    range.addEventListener('mousemove', handleRangeUpdate);
 });
 
 let mousedown = false;
-progress.addEventListener("click", scrub);
+progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e))
-progress.addEventListener("mousedown", () => mousedown = true);
+progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
